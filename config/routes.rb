952248e3c9b427c4c43014b43a17fb000716fee5
Mike Cecconello <mikececco2000@gmail.com>
2023-03-20 19:33:32 +0100

    Added restaurant API controller and policy - base controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :restaurants, only: [:index]
+    end
+  end
 end
